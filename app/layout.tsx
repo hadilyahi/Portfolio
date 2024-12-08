e97f6feffbecc9ea6b657c6e852feeacff88f8ce
Hadil Yahi <165954236+hadilyahi@users.noreply.github.com>
2024-12-08 20:09:05 +0100

    create navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 
 const jetBrainsMono = JetBrains_Mono({
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
    
       <body className={jetBrainsMono.variable}>
+        <Header/>
         {children}
       </body>
     </html>
